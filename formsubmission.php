d93cdcd8a22c25e5d4840372745c134a97b95cbe
Luke Silver <31713340+Luke-Ag@users.noreply.github.com>
2023-10-02 20:51:52 -0400

    Add files via upload

---
+++ formsubmission.php
@@ -0,0 +1,13 @@
+<?php 
+  $0= $_POST["physical"]; 
+  $1= $_POST["mental"]; 
+  $2= $_POST["medchange"];
+  $3= $_POST["recentid"]; 
+  $4= $_POST["burden"]; 
+  $5= $_POST["killswitch"];
+  $6= $_POST["alone"]; 
+  $7= $_POST["killswitch2"]; 
+  $8= $_POST["sleep"]; 
+  $9= $_POST["ewie"];
+  $result_message="physical mental medchange recentid burden killswitch alone killswitch2 sleep ewie";
+?>
